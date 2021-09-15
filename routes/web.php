89799a1d055dd6d58bf94c6d091ac31c893ca4a2
kholid saifulloh <muhammadkholid@refactory.id>
2021-09-15 17:07:00 +0700

    learn routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Kholid Saifulloh",
+        "email" => "muhammadkholid@refactory.id",
+        "image" => "me.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
